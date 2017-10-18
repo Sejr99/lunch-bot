2da147c34530759c7b0b13d2b54a98310a57965d
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-10-18 19:56:58 +0200

    Parsing data into a message

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const lunchApi = require('./lib/api/lunch')
 const lunchParser = require('./lib/parse/lunchPage')
+const slackMessage = require('./lib/message/slack');
+
+const days = 5;
 
 const getLunch = () =>
   lunchApi
     .getLunchPage()
     .then(lunchParser.parseLunchPage)
-    .then(JSON.stringify)
+    .then((lunchList) => slackMessage.getSlackMessage(lunchList, days))
     .then(console.log)
 
 getLunch()

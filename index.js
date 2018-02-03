e6ed31eb652e4dd728e7573a1bcc6e1a030aa4fe
Mikael Midtgaard <mikaelmidt@gmail.com>
2018-02-03 15:15:06 +0100

    Use google images to find an image of the lunch

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Botkit = require('botkit')
+const GoogleImages = require('google-images');
+
 const lunchApi = require('./lib/api/lunch')
 const lunchParser = require('./lib/parse/lunchPage')
 const slackMessage = require('./lib/message/slack')
+
+const searchClient = new GoogleImages(process.env.SEARCH_ENGINE_ID, process.env.GOOGLE_API_KEY);
 
 // init slack bot
 const lunchBot = Botkit.slackbot({
@@ -24,13 +28,25 @@ lunchBot
 lunchBot.on('direct_mention', (bot, message) => {
   console.log(message)
   const parsedMessage = { days: 1 }
-
+  let firstMainCourse;
   lunchApi
     .getLunchPage()
     .then(lunchParser.parseLunchPage)
-    .then(lunchList =>
-      slackMessage.getSlackMessage(lunchList, parsedMessage.days)
+    .then(lunchList => {
+      firstMainCourse = lunchList.find(item => item.header.toUpperCase() === 'DAGENS VARME RET');
+      return slackMessage.getSlackMessage(lunchList, parsedMessage.days)
+      }
     )
     .then(text => bot.reply(message, text))
+    .then(() => {
+      if (firstMainCourse && firstMainCourse.description) {
+        return searchClient.search(firstMainCourse.description)
+      }
+    })
+    .then((searchResults) => {
+      if (searchResults.length && searchResults[0].url) {
+        bot.reply(message, searchResults[0].url)
+      }
+    })
     .catch(error => console.log('error: ', error))
 })
